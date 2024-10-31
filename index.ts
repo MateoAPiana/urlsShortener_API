b6ccb895aa2ecb5ead832cff32c388987dfd1223
M. Andrés Piana <153022165+MateoAPiana@users.noreply.github.com>
2024-10-31 11:24:37 -0300

    create shorted urls

--- index.ts
+++ index.ts
@@ -1,11 +1,23 @@
 import express from "express"
+import morgan from "morgan"
+import cors from "cors"
+import { createNewURLShorted } from "./models/urls"
 
 const app = express()
 
 const PORT = process.env.PORT || 3000
 
-app.get("/", (req, res) => {
-  res.send("Hello")
+app.use(express.json())
+
+app.use(morgan("dev"))
+
+app.use(cors())
+
+app.post("/", async (req, res) => {
+  const { url } = req.body
+  const dbRes = await createNewURLShorted({ url })
+  if (dbRes.error) res.status(400).json({ error: dbRes.error })
+  else res.status(201).json({ url, newURL: dbRes.newURL })
 })
 
 app.listen(PORT, () => {
--- models/urls.ts
+++ models/urls.ts
@@ -2,11 +2,11 @@ import { PrismaClient } from "@prisma/client"
 
 const prisma = new PrismaClient()
 
-export function createNewURLShorted({ url }) {
+export async function createNewURLShorted({ url }) {
   try {
     const id = crypto.randomUUID()
-    const newURL = `${process.env.PAGE_URL}/${id}`
-    prisma.uRLRegister.create({
+    const newURL = `${process.env.PAGE_URL || "http://localhost:3000"}/${id}`
+    await prisma.uRLRegister.create({
       data: {
         url_original: url,
         url_shorted: newURL
